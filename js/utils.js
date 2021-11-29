cc435e2a980d4d7e4a08347ec6d2fab4f984f359
Marco Messina <voldrak94@gmail.com>
2021-11-29 15:35:23 +0100

    feat: aggiunto input di ricerca

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
-const API = "https://jsonplaceholder.typicode.com/users";
+const api = "https://jsonplaceholder.typicode.com/users";
 
 const render = (container, content) => (container.innerHTML = content);
 
-export { API, render };
+export { api, render };
